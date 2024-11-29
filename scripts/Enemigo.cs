30a86d3cd7eff2491656430428f309d0b6917bbb
ilopjim1306 <ljivan@iesrafaelalberti.es>
2024-11-29 20:46:09 +0100

    retoques en clase

--- scripts/Enemigo.cs
+++ scripts/Enemigo.cs
@@ -51,7 +51,7 @@ public partial class Enemigo : Area2D
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
 		
 		if (!isDeath) {
